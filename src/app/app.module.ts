ead0d5caf45d88ec34ebb959ca7cc65f950953c2
abdoul <abdoul@localhost.localdomain>
2023-03-09 12:16:31 +0000

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { LoginComponent } from './login/login.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { TableauComponent } from './components/tableau/tableau.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    LoginComponent,
+    DashbordComponent,
+    TableauComponent
   ],
   imports: [
     BrowserModule,
