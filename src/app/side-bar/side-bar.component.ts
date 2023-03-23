8b10ef7ff1d220f6170073164dfdcf34bd863d8d
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2023-03-23 04:23:32 +0100

    sidebar

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit{
+  
+  switchRoof:boolean = false;
+  fan:boolean = false;
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  switchToit(){
+    this.switchRoof? this.switchRoof= false: this.switchRoof = true;
+  }
+
+  switchFan(){
+    this.fan? this.fan= false: this.fan = true;
+  }
 }
