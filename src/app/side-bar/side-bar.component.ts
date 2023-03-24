c3d7b95d30aec6099a7bd7098f388c5c9b6762ca
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2023-03-24 03:59:21 +0100

    side && navbar

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -9,6 +9,8 @@ export class SideBarComponent implements OnInit{
   
   switchRoof:boolean = false;
   fan:boolean = false;
+  showDashboard:boolean = true;
+  showInfo:boolean = false;
   
   ngOnInit(): void {
     throw new Error('Method not implemented.');
@@ -20,5 +22,13 @@ export class SideBarComponent implements OnInit{
 
   switchFan(){
     this.fan? this.fan= false: this.fan = true;
+  }
+
+  switchInfo () {
+    if (this.showDashboard == true){
+      this.showInfo = false;
+    }else{
+      this.showDashboard = false;
+    }
   }
 }
