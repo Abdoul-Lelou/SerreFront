484b2169ac2b426aa4274ee512e3e1c295cc9f2c
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2023-03-24 03:27:06 +0100

    side && navbar

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -9,8 +9,12 @@ export class SideBarComponent implements OnInit{
   
   switchRoof:boolean = false;
   fan:boolean = false;
+  currentDate: any;
+
+  constructor(){}
   
   ngOnInit(): void {
+    this.showTime();
     throw new Error('Method not implemented.');
   }
 
@@ -20,5 +24,14 @@ export class SideBarComponent implements OnInit{
 
   switchFan(){
     this.fan? this.fan= false: this.fan = true;
+  }
+
+  showTime(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    this.currentDate= mm + '/' + dd + '/' + yyyy;
   }
 }
