df928ea0668fc695113eb2bcf4c2be012f06b30e
Abdoul-lelou <abdourahmane.benzy@gmail.com>
2023-03-27 11:02:26 +0200

    reglage des branches

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InfosSerreComponent } from './pages/infos-serre/infos-serre.component';
+// import { InfosSerreComponent } from './pages/infos-serre/infos-serre.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
